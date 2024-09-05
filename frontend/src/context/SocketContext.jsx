6da08044546af17e0dedb2a4962d358fdf8b0eef
revising <vipanshu.srivastava28@gmail.com>
2024-09-05 14:20:03 +0530

    Deployed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:2001", {
+			const socket = io("https://lets-talk-yk8x.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
